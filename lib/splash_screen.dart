e40c42604262fda22ae362c1c4d44e5613986090
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-01-31 17:09:54 +0530

    image changed for splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,20 +10,36 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
 
-  SplashServics splashServics = SplashServics();
+  // SplashServics splashServics = SplashServics();
 
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    splashServics.isLogin(context);
-  }
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+  //   splashServics.isLogin(context);
+  // }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Firebase", style: TextStyle(fontSize: 30),),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset("images/checksign2.png"),
+          ),
+          const SizedBox(height: 20,),
+          Container(
+            child: const Text(
+              "Attendance Application",
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w500
+              ),
+            )
+          )
+        ],
       ),
     );
   }
