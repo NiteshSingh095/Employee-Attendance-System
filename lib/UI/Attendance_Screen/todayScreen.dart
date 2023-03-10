71c16fd108bea25495ccff872e55ff7690df5f45
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-03-10 22:57:35 +0530

    firebase checkIn and checkOut system update

--- lib/UI/Attendance_Screen/todayScreen.dart
+++ lib/UI/Attendance_Screen/todayScreen.dart
@@ -1,9 +1,17 @@
+import 'package:attendance_system/Firebase_Services/splash_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:slide_to_act/slide_to_act.dart';
 
+import '../../modal/user.dart';
+
 class todayScreen extends StatefulWidget {
-  const todayScreen({Key? key}) : super(key: key);
+
+  String ? emailId = "";
+
+  todayScreen({Key? key, required this.emailId}) : super(key: key);
 
   @override
   State<todayScreen> createState() => _todayScreenState();
@@ -15,6 +23,8 @@ class _todayScreenState extends State<todayScreen> {
   double screenWidth = 0;
 
   Color primary = const Color(0xffeef444c);
+
+  late String ? empId = Users.username;
 
   @override
   Widget build(BuildContext context) {
@@ -41,7 +51,7 @@ class _todayScreenState extends State<todayScreen> {
             Container(
               alignment: Alignment.centerLeft,
               child: Text(
-                "Employee",
+                "Employee : " + "$empId",
                 style: TextStyle(
                     color: Colors.black54,
                     fontSize: screenWidth / 18,
@@ -173,9 +183,53 @@ class _todayScreenState extends State<todayScreen> {
                   innerColor: primary,
                   outerColor: Colors.white,
                   key: key,
-                  onSubmit: (){
+                  onSubmit: () async {
                     key.currentState!.reset();
 
+                    // print(DateFormat("dd MMMM yyyy").format(DateTime.now()));
+
+                    QuerySnapshot snap = await FirebaseFirestore.instance
+                        .collection("Employee")
+                        .where('id', isEqualTo: empId)
+                        .get();
+                    //
+                    print(snap.docs[0].id);
+
+                    DocumentSnapshot snap2 = await FirebaseFirestore.instance
+                        .collection("Employee")
+                        .doc(snap.docs[0].id)
+                        .collection("Record")
+                        .doc(DateFormat("dd MMMM yyyy").format(DateTime.now()))
+                        .get();
+
+                    try
+                        {
+                          String checkIn = DateFormat("hh : mm").format(DateTime.now());
+                          await FirebaseFirestore.instance
+                              .collection("Employee")
+                              .doc(snap.docs[0].id)
+                              .collection("Record")
+                              .doc(DateFormat("dd MMMM yyyy").format(DateTime.now()))
+                              .update(
+                              {
+                                'checkIn' : checkIn,
+                                'checkOut' : DateFormat("hh : mm").format(DateTime.now())
+                              }
+                          );
+                        }
+                        catch(e)
+                    {
+                      await FirebaseFirestore.instance
+                          .collection("Employee")
+                          .doc(snap.docs[0].id)
+                          .collection("Record")
+                          .doc(DateFormat("dd MMMM yyyy").format(DateTime.now()))
+                          .set(
+                          {
+                            'checkIn' : DateFormat("hh : mm").format(DateTime.now())
+                          }
+                      );
+                    }
                   },
                 );
               }),
