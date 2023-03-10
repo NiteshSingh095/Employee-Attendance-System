5e230efc4f62916430d63c528f5e8a17d6da9a3f
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-03-10 18:18:02 +0530

    authentication services updated

--- lib/Firebase_Services/splash_services.dart
+++ lib/Firebase_Services/splash_services.dart
@@ -1,20 +1,30 @@
 import 'dart:async';
+import 'package:attendance_system/UI/Attendance_Screen/HomeScreen.dart';
+import 'package:attendance_system/UI/Attendance_Screen/profileScreen.dart';
 import 'package:attendance_system/UI/Authentication/Login_Screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashServics
 {
-  void isLogin(BuildContext context)
+  void isLogin(BuildContext context) async
   {
 
-    final user = 'Nitesh';
+    FirebaseAuth _auth = FirebaseAuth.instance;
 
-    if(user != null)
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+
+    final user2 = sharedPreferences.get("employeeId").toString();
+
+    final user1 = _auth.currentUser;
+
+    if(user1 != null && user2 != null)
       {
         Timer(
             const Duration(seconds: 3),
-                () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginScreen()))
+                () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => HomeScreen(emailId: user2,)))
         );
       }
     else
