127aa47cc192a19964e85279687d7570c23c240c
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-03-12 00:59:34 +0530

    working on  calendar screen

--- lib/UI/Attendance_Screen/calendarScreen.dart
+++ lib/UI/Attendance_Screen/calendarScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class calendarScreen extends StatefulWidget {
   const calendarScreen({Key? key}) : super(key: key);
@@ -8,12 +9,64 @@ class calendarScreen extends StatefulWidget {
 }
 
 class _calendarScreenState extends State<calendarScreen> {
+
+  double screenHeight = 0;
+  double screenWidth = 0;
+
+  Color primary = const Color(0xffeef444c);
+
   @override
   Widget build(BuildContext context) {
+
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: const Center(
-        child: Text("Calendar Screen"),
-      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+
+            // This container contains "My attendance" text
+            Container(
+              alignment: Alignment.centerLeft,
+              margin: const EdgeInsets.only(top: 32),
+              child: Text(
+                "My Attendance",
+                style: TextStyle(
+                    color: Colors.black, fontSize: screenWidth / 18, fontWeight: FontWeight.w600),
+              ),
+            ),
+
+            Stack(
+              children: [
+
+                // This container contains the month text
+                Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Text(
+                    DateFormat("MMMM").format(DateTime.now()),
+                    style: TextStyle(
+                        color: Colors.black, fontSize: screenWidth / 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+
+                //This container contains the month picker
+                Container(
+                  alignment: Alignment.centerRight,
+                  margin: const EdgeInsets.only(top: 32),
+                  child: Text(
+                    "Pick a Month",
+                    style: TextStyle(
+                        color: Colors.black, fontSize: screenWidth / 18, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
