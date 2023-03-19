c620de6c6bb90cbf5bc56e8d650313bd707e25e9
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-03-19 11:11:10 +0530

    working on profile screen

--- lib/UI/Attendance_Screen/profileScreen.dart
+++ lib/UI/Attendance_Screen/profileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:attendance_system/modal/user.dart';
 import 'package:flutter/material.dart';
 
 class profileScreen extends StatefulWidget {
@@ -8,12 +9,62 @@ class profileScreen extends StatefulWidget {
 }
 
 class _profileScreenState extends State<profileScreen> {
+
+  Color primary = const Color(0xffeef444c);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: Text("Profile Screen"),
-      ),
+      body: Column(
+        children: [
+
+          // This container will store the picture of employee
+          Container(
+            margin: const EdgeInsets.only(top: 80, bottom: 20),
+            alignment: Alignment.center,
+            height: 120,
+            width: 100,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: primary
+            ),
+
+            child: const Center(
+              child : Icon(
+                  Icons.person,
+                color: Colors.white,
+                size: 80,
+              )
+            ),
+          ),
+
+          // This contains the text for employee Id
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+                "Employee : " + Users.employeeId,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500
+              ),
+            ),
+          ),
+
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: "hint",
+              hintStyle: TextStyle(
+                color: Colors.black54
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black54
+                )
+              )
+            ),
+          )
+        ],
+      )
     );
   }
 }
