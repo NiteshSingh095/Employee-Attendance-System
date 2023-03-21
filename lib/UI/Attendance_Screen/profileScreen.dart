e0c1d0066deec49bdc37ad397e7ba717539ee8f8
Nitesh Kumar Singh <anshukumargautam095@gmail.com>
2023-03-21 22:07:22 +0530

    working on user edit option

--- lib/UI/Attendance_Screen/profileScreen.dart
+++ lib/UI/Attendance_Screen/profileScreen.dart
@@ -31,164 +31,174 @@ class _profileScreenState extends State<profileScreen> {
     return Scaffold(
       body: SingleChildScrollView(
 
-        child: Column(
-          children: [
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
 
-            // This container will store the picture of employee
-            Container(
-              margin: const EdgeInsets.only(top: 45, bottom: 20),
-              alignment: Alignment.center,
-              height: 120,
-              width: 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: primary
-              ),
-
-              child: const Center(
-                child : Icon(
-                    Icons.person,
-                  color: Colors.white,
-                  size: 80,
-                )
-              ),
-            ),
-
-            // This contains the text for employee Id
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                  "Employee : " + Users.employeeId,
-                style: const TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 15,),
-
-            // This field is used to get the first name of employee
-            textField("First Name","First Name", firstNameController),
-
-          //  This field is used to get the last name of employee
-            textField("Last Name","Last Name", lastNameController),
-
-            // This container contains the constant Date of Birth text
-            const Padding(
-              padding: EdgeInsets.only(top: 10.0, left: 14.0, bottom: 4),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text("Date of Birth", style: const TextStyle(fontWeight: FontWeight.w500),),
-              ),
-            ),
-
-            // This container contains the date of birth field
-            GestureDetector(
-              onTap: (){
-                showDatePicker(
-                    context: context,
-                    initialDate: DateTime.now(),
-                    firstDate: DateTime(1900),
-                    lastDate: DateTime.now(),
-                    builder: (context, child)
-                    {
-                      return Theme(
-                          data: Theme.of(context).copyWith(
-                              colorScheme: ColorScheme.light(
-                                  primary: primary,
-                                  secondary: primary,
-                                  onSecondary: Colors.white
-                              ),
-                              textTheme: const TextTheme(
-                                headline4: TextStyle(fontWeight: FontWeight.bold),
-                                overline: TextStyle(fontWeight: FontWeight.w700),
-                                button: TextStyle(fontWeight: FontWeight.w700),
-                              )
-                          ),
-                          child: child!
-                      );
-                    }
-                ).then((value)
-                {
-                  setState(() {
-                    birth = DateFormat("dd-MM-yyyy").format(value!);
-                  });
-                });
-              },
-              child: Container(
-                margin: const EdgeInsets.only(left: 13, right: 13),
-                height: kTextTabBarHeight*1.15,
-                width: screenWidth,
+              // This container will store the picture of employee
+              Container(
+                margin: const EdgeInsets.only(top: 45, bottom: 20),
+                alignment: Alignment.center,
+                height: 120,
+                width: 100,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(4),
-                  border: Border.all(color: Colors.black54)
+                  borderRadius: BorderRadius.circular(20),
+                  color: primary
                 ),
-                child: Container(
-                  padding: const EdgeInsets.only(left: 10),
+
+                child: const Center(
+                  child : Icon(
+                      Icons.person,
+                    color: Colors.white,
+                    size: 80,
+                  )
+                ),
+              ),
+
+              // This contains the text for employee Id
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                    "Employee : " + Users.employeeId,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 15,),
+
+              // This field is used to get the first name of employee
+              textField("First Name","First Name", firstNameController),
+
+            //  This field is used to get the last name of employee
+              textField("Last Name","Last Name", lastNameController),
+
+              // This container contains the constant Date of Birth text
+              const Padding(
+                padding: EdgeInsets.only(top: 10.0, left: 14.0, bottom: 4),
+                child: Align(
                   alignment: Alignment.centerLeft,
-                  child:Text(birth, style: const TextStyle(color: Colors.black54, fontSize: 16),),
+                  child: Text("Date of Birth", style: const TextStyle(fontWeight: FontWeight.w500),),
                 ),
               ),
-            ),
 
-            // This field is used to store address of employee
-            textField("Address","Address", addressController),
-
-            // This button is used to save all the information present in above fields
-            GestureDetector(
-              onTap: () async
-              {
-                String firstName = firstNameController.text.toString();
-                String lastName = lastNameController.text.toString();
-                String birth1 = birth.toString();
-                String address = addressController.text.toString();
-
-                if(firstName.isEmpty)
+              // This container contains the date of birth field
+              GestureDetector(
+                onTap: (){
+                  showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(1900),
+                      lastDate: DateTime.now(),
+                      builder: (context, child)
+                      {
+                        return Theme(
+                            data: Theme.of(context).copyWith(
+                                colorScheme: ColorScheme.light(
+                                    primary: primary,
+                                    secondary: primary,
+                                    onSecondary: Colors.white
+                                ),
+                                textTheme: const TextTheme(
+                                  headline4: TextStyle(fontWeight: FontWeight.bold),
+                                  overline: TextStyle(fontWeight: FontWeight.w700),
+                                  button: TextStyle(fontWeight: FontWeight.w700),
+                                )
+                            ),
+                            child: child!
+                        );
+                      }
+                  ).then((value)
                   {
-                    showSnackbar("Enter your first Name");
-                  }
-                else if(lastName.isEmpty)
-                  {
-                    showSnackbar("Enter your last Name");
-                  }
-                else if(birth1.isEmpty)
-                  {
-                    showSnackbar("Select Date of Birth");
-                  }
-                else if(address.isEmpty)
-                  {
-                    showSnackbar("Enter your address");
-                  }
-                else
-                  {
-
-                  }
-              },
-              child: Container(
-                margin: const EdgeInsets.only(left: 13, right: 13, top: 10),
-                height: kTextTabBarHeight,
-                width: screenWidth,
-                decoration: BoxDecoration(
-                  color: primary,
+                    setState(() {
+                      birth = DateFormat("dd-MM-yyyy").format(value!);
+                    });
+                  });
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(left: 13, right: 13),
+                  height: kTextTabBarHeight*1.15,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(4),
                     border: Border.all(color: Colors.black54)
-                ),
-                child: Center(
+                  ),
                   child: Container(
-                    child: const Text(
-                      "Save",
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500
+                    padding: const EdgeInsets.only(left: 10),
+                    alignment: Alignment.centerLeft,
+                    child:Text(birth, style: const TextStyle(color: Colors.black54, fontSize: 16),),
+                  ),
+                ),
+              ),
+
+              // This field is used to store address of employee
+              textField("Address","Address", addressController),
+
+              // This button is used to save all the information present in above fields
+              GestureDetector(
+                onTap: () async
+                {
+                  String firstName = firstNameController.text.toString();
+                  String lastName = lastNameController.text.toString();
+                  String birth1 = birth.toString();
+                  String address = addressController.text.toString();
+
+                  if(Users.canEdit)
+                    {
+                      if(firstName.isEmpty)
+                      {
+                        showSnackbar("Enter your first Name");
+                      }
+                      else if(lastName.isEmpty)
+                      {
+                        showSnackbar("Enter your last Name");
+                      }
+                      else if(birth1.isEmpty)
+                      {
+                        showSnackbar("Select Date of Birth");
+                      }
+                      else if(address.isEmpty)
+                      {
+                        showSnackbar("Enter your address");
+                      }
+                      else
+                      {
+
+                      }
+                    }
+                  else
+                    {
+                      showSnackbar("You can\'t edit anymore. Contact support team!");
+                    }
+                },
+                child: Container(
+                  margin: const EdgeInsets.only(left: 13, right: 13, top: 10),
+                  height: kTextTabBarHeight,
+                  width: screenWidth,
+                  decoration: BoxDecoration(
+                    color: primary,
+                      borderRadius: BorderRadius.circular(4),
+                      border: Border.all(color: Colors.black54)
+                  ),
+                  child: Center(
+                    child: Container(
+                      child: const Text(
+                        "Save",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500
+                        ),
                       ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )
     );
